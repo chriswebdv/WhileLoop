4db115ff6090efb59a646d2d383eea8cfd607400
mypc <mypc@CLAGOS>
2023-09-02 18:03:03 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿bool isCompOn = true;
+
+while (isCompOn == true)
+{
+    Console.WriteLine("Type yes is computer is on, Type no is not");
+    string userInput = Console.ReadLine();
+
+    if (userInput == "yes")
+    {
+        Console.WriteLine("Correcto mundo!");
+        isCompOn = false;
+    }
+    else
+    {
+        Console.WriteLine("Try again");
+    }
+}
